fd1d3daf77d1662803266d373a067e8da184de42
Eric Harvey <eharvey2@gmail.com>
2020-03-27 09:43:18 -0700

    one more time

--- infrmdcr.rb
+++ infrmdcr.rb
@@ -1,10 +1,10 @@
 class Infrmdcr < Formula
   desc 'A general purpose utility for Informed internal use'
-  homepage 'https://github.com/veelenga/ameba'
+  homepage 'https://github.com/EricHarvey/homebrew-informed'
   url 'https://github.com/EricHarvey/homebrew-informed/archive/v0.11.0.tar.gz'
   version '0.1.0'
   sha256 'cc72e330b53f684945f0ea1f9396266804aee1bb'
-  head 'https://github.com/veelenga/ameba.git'
+  head 'https://github.com/EricHarvey/homebrew-informed.git'
 
   depends_on 'crystal-lang' => :build
 
