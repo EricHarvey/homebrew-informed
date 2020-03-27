64505f869f9f4218b654c1768b54bbf10ee594b4
Eric Harvey <eharvey2@gmail.com>
2020-03-27 12:19:07 -0700

    test

--- infrmdcr.rb
+++ infrmdcr.rb
@@ -3,7 +3,7 @@ require_relative './lib/custom_download_strategy.rb'
 class Infrmdcr < Formula
   desc 'A general purpose utility for Informed internal use'
   homepage 'https://github.com/EricHarvey/homebrew-informed'
-  url 'https://github.com/Informed/infrmdcr/releases/download/0.1.0/v0.1.0.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url 'https://github.com/Informed/infrmdcr/releases/download/0.1.1/v0.1.1.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   version '0.1.0'
   sha256 '489be71685ff139837778d16f3e32598a5d4614170b3716346031005f9ad1f58'
   head 'https://github.com/EricHarvey/homebrew-informed.git'
@@ -12,5 +12,6 @@ class Infrmdcr < Formula
 
   def install
     `./build.sh`
+    bin.install './infrmdcr'
   end
 end
