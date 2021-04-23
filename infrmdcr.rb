fa21f88a625b3547b74ebc2219f9211e0c618644
Eric Harvey <eharvey2@gmail.com>
2021-04-23 10:47:22 -0700

    will this work?

--- infrmdcr.rb
+++ infrmdcr.rb
@@ -4,7 +4,7 @@ class Infrmdcr < Formula
   desc 'A general purpose utility for Informed internal use'
   homepage 'https://github.com/EricHarvey/homebrew-informed'
   url 'https://github.com/Informed/infrmdcr/releases/download/v0.2.4/v0.2.4.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  version '0.2.4'
+  version '0.2.4.patch'
   sha256 '22677b174360124b3818fe4cca38a0f95eb9d892c8cd07f7aed5e70473dda82f'
   head 'https://github.com/EricHarvey/homebrew-informed.git'
 
