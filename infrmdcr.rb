04fe64d60ad6a99e5defd66f8f78445513ef3ee6
Eric Harvey <eharvey2@gmail.com>
2021-07-01 13:14:10 -0700

    Patch

--- infrmdcr.rb
+++ infrmdcr.rb
@@ -4,7 +4,7 @@ class Infrmdcr < Formula
   desc 'A general purpose utility for Informed internal use'
   homepage 'https://github.com/EricHarvey/homebrew-informed'
   url 'https://github.com/Informed/infrmdcr/releases/download/v0.2.7/v0.2.7.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  version '0.2.7'
+  version '0.2.7-patch-1'
   sha256 'bf94d92b04ce043b2a7ae6435205b01cba3341e1e2eeed9af0ef0ad0120cff93'
   head 'https://github.com/EricHarvey/homebrew-informed.git'
 
