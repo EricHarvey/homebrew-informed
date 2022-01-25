f5425bbe13bfe85c74b05aadaebf939d9a6581bd
Eric Harvey <eharvey2@gmail.com>
2022-01-24 18:06:25 -0800

    v0.4.0

--- infrmdcr.rb
+++ infrmdcr.rb
@@ -3,9 +3,9 @@ require_relative './lib/custom_download_strategy.rb'
 class Infrmdcr < Formula
   desc 'A general purpose utility for Informed internal use'
   homepage 'https://github.com/EricHarvey/homebrew-informed'
-  url 'https://github.com/Informed/infrmdcr/releases/download/v0.3.0/v0.3.0.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
-  version '0.3.0'
-  sha256 'e173adc8fd74d0d49ecdf68877ca083ae76e3768df45d26f51c531bae768a7e2'
+  url 'https://github.com/Informed/infrmdcr/releases/download/v0.4.0/v0.4.0.zip', :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  version '0.4.0'
+  sha256 'f792eb00db81707c85d65a2ce4b57aad939a5b7206ffc9a63f66fdd7a0a1a4b1'
   head 'https://github.com/EricHarvey/homebrew-informed.git'
 
   depends_on 'bdw-gc'
